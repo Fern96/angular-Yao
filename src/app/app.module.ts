6cfda0e42173da97fb1a8699e80243c49a6cfccb
[yaojiafeng] <[274764538@qq.com]>
2017-11-19 12:00:40 +0800

    commit again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,16 @@ import { ResumeComponent } from './resume/resume.component';
 import { HomeComponent } from './home/home.component';
 import { StudyComponent } from './study/study.component';
 import { InterestComponent } from './interest/interest.component';
-import { FamilyComponent } from './family/family.component';
 import { PhotosComponent } from './photos/photos.component';
-import { WorksComponent } from './works/works.component'
+import { WorksComponent } from './works/works.component';
+import { CommunicationComponent } from './communication/communication.component'
 const routes:Routes=[
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'resume',component:ResumeComponent},
   {path:'interest',component:InterestComponent},
-  {path:'family',component:FamilyComponent},
+  {path:'communication',component:CommunicationComponent},
+
   {path:'study',component:StudyComponent},
   {path:'photos',component:PhotosComponent},
   {path:'works',component:WorksComponent}
@@ -31,9 +32,9 @@ const routes:Routes=[
     HomeComponent,
     StudyComponent,
     InterestComponent,
-    FamilyComponent,
     PhotosComponent,
-    WorksComponent
+    WorksComponent,
+    CommunicationComponent
   ],
   imports: [
     BrowserModule,
