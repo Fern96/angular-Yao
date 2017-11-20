634f62442e8c09aba7ac73252cd1d45307bdd41f
[yaojiafeng] <[274764538@qq.com]>
2017-11-20 19:01:50 +0800

    updata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes , RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
@@ -9,19 +9,15 @@ import { HomeComponent } from './home/home.component';
 import { StudyComponent } from './study/study.component';
 import { InterestComponent } from './interest/interest.component';
 import { PhotosComponent } from './photos/photos.component';
-import { WorksComponent } from './works/works.component';
-import { CommunicationComponent } from './communication/communication.component'
-const routes:Routes=[
-  {path:'',redirectTo:'/home',pathMatch:'full'},
-  {path:'home',component:HomeComponent},
-  {path:'resume',component:ResumeComponent},
-  {path:'interest',component:InterestComponent},
-  {path:'communication',component:CommunicationComponent},
-
-  {path:'study',component:StudyComponent},
-  {path:'photos',component:PhotosComponent},
-  {path:'works',component:WorksComponent}
- 
+import { WorksComponent } from './works/works.component'
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'interest', component: InterestComponent },
+  { path: 'study', component: StudyComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'works', component: WorksComponent },
 ];
 
 @NgModule({
@@ -33,8 +29,7 @@ const routes:Routes=[
     StudyComponent,
     InterestComponent,
     PhotosComponent,
-    WorksComponent,
-    CommunicationComponent
+    WorksComponent
   ],
   imports: [
     BrowserModule,
